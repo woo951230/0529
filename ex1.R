60a0e861735b68c148ab3a312184a86585002ae2
woo951230 <musk930107@example.com>
2020-06-05 02:12:02 +0000

    0605

---
+++ ex1.R
@@ -0,0 +1,9 @@
+filename='108新生註冊率.csv
+x=read.csv(filename,fileEncoding = "utf8")
+summary(x$當學年度新生註冊率...D..C..A.B...100.)
+boxplot(x$當學年度新生註冊率...D..C..A.B...100.)
+
+summary(x$當學年度新生註冊率...D..C..A.B...100.)
+summary(x$當學年度新生註冊率...D..C..A.B...100.)
+boxplot(x$當學年度新生註冊率...D..C..A.B...100.)
+head(x[order(x$當學年度新生註冊率...D..C..A.B...100.),])
